5fca7546b79e07d34293f0e3010837a6d84d46fd
Медведев Дмитрий Александрович <dmedvedev01@dellin.local>
2020-09-21 00:30:18 +0300

    базовые файлы

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CanonicalSpellingKeywordsSDBLDiagnostic.java
@@ -0,0 +1,28 @@
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticInfo;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.parser.SDBLParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.INFO,
+  minutesToFix = 1,
+  tags = {
+    DiagnosticTag.STANDARD
+  }
+
+)
+public class CanonicalSpellingKeywordsSDBLDiagnostic extends AbstractSDBLVisitorDiagnostic {
+
+  @Override
+  public ParseTree visitStatement(SDBLParser.StatementContext ctx) {
+
+    diagnosticStorage.addDiagnostic(ctx.literal);
+    return super.visitStatement(ctx);
+  }
+}
